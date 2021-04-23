9ce6daa3049bf509bf311f2e4e518e419bdab703
u2008405 <Josh.Davies.1@warwick.ac.uk>
2021-04-23 18:27:34 +0100

    Remove newsletter app

--- newsletter/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from .models import Mail
-
-
-class MailModelForm(ModelForm):
-    class Meta:
-        model = Mail
-        fields = ['subject', 'sender_name', 'sender_email', 'text']

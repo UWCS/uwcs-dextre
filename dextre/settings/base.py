7d724dedd3a4dd8455b5e448feab1abe2b0f32a9
Josh Davies <Joshua.T.Davies@warwick.ac.uk>
2021-12-23 22:44:59 +0000

    Add django-saml-sp as base for integration with Warwick WSO

--- dextre/settings/base.py
+++ dextre/settings/base.py
@@ -60,12 +60,18 @@ INSTALLED_APPS = [
 
     'oauth2_provider',
     'corsheaders',
+    'sp',
 
     'djangobower',
     'compressor',
     'anymail',
     'markdownx',
     'widget_tweaks',
+]
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'sp.backends.SAMLAuthenticationBackend'
 ]
 
 REST_FRAMEWORK = {

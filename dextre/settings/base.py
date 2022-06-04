cbcf6dfbafa89d303571f66c2acf4ae3412d280b
Josh Davies <josh@joshdavies.tech>
2022-06-04 20:31:45 +0100

    :wrench: Add `DEFAULT_AUTO_FIELD` setting to remove startup warnings

--- dextre/settings/base.py
+++ dextre/settings/base.py
@@ -124,6 +124,8 @@ DATABASES = {
     }
 }
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 LANGUAGE_CODE = "en-gb"

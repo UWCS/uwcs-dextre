8f67001a339a5a466f6278c107e9123846d55699
Josh Davies <josh@joshdavies.tech>
2022-09-07 16:09:25 +0100

    :adhesive_bandage: If night mode turned on, turn off auto theme

--- accounts/views.py
+++ accounts/views.py
@@ -175,9 +175,9 @@ class MemberAccountUpdateDoneView(LoginRequiredMixin, View):
 
 class ToggleNightModeView(View):
     def post(self, request):
-        request.session["night_mode"] = (
-            request.POST.get("night_mode", default="") == "true"
-        )
+        val = request.POST.get("night_mode", default="") == "true"
+        request.session["night_mode"] = val
+        request.session["auto_colour_scheme"] = not val
 
         return HttpResponse(status=200)
 
